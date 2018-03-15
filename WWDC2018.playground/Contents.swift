fca95cbc06c350ae00ae4f7aaa4c9858c5ecdc08
Skyler Svendsen <skylersvendsen@gmail.com>
2018-03-14 21:59:02 -0500

    rearranged scene call

--- WWDC2018.playground/Contents.swift
+++ WWDC2018.playground/Contents.swift
@@ -91,9 +91,13 @@ let frame = CGRect(x: 0, y: 0, width: 500, height: 750)
 let midpoint = CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0)
 let treePosition = CGPoint(x: midpoint.x, y: midpoint.y + frame.size.height * 0.25)
 
+let view = SKView(frame: frame)
+var scene = SKScene(size: frame.size)
+scene.backgroundColor = UIColor.lightGray
+
 func createTree() {
     let tree = SKSpriteNode(imageNamed: "Tree")
-    
+
     tree.position = treePosition
     tree.setScale(0.2)
     scene.addChild(tree)
@@ -112,15 +116,12 @@ func createTree() {
 //    }
 //}
 
-var scene = SKScene(size: frame.size)
-scene.backgroundColor = UIColor.lightGray
-
 createTree()
 //createHearts()
 
-let view = SKView(frame: frame)
 view.presentScene(scene)
 PlaygroundPage.current.liveView = view
+
 
 
 
